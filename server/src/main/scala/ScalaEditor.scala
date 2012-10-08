e500e32f7a847bda68e3ebe3451d3b9666b4cff1
vascogrilo <vascogrilo@gmail.com>
2012-10-08 21:45:21 +0100

    Using ScalaIntepreter's Interpreter class for evaluating the code. It doesn't print out the results yet.

--- server/src/main/scala/ScalaEditor.scala
+++ server/src/main/scala/ScalaEditor.scala
@@ -4,28 +4,39 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.netty._
 
-import com.twitter.util._
 import scala.xml._
+import de.sciss.scalainterpreter._
+
+import collection.mutable._
 
 object ScalaEditor extends ServerPlan2 {
 	
 	import QParams._
 	val logger = org.clapper.avsl.Logger(getClass)
 	
+	val intpCfg = de.sciss.scalainterpreter.Interpreter.Config()
+	val interpreter = de.sciss.scalainterpreter.Interpreter(intpCfg)
+	
 	def intent = {
 		case GET(Path("/scala")) =>
 			Redirect("/editor")
 		case GET(Path("/editor")) => 
 			logger.debug("GET /editor")
-			Eval.compile("object Session { }")
+			//Eval.compile("object Session { }")
 			EditorView.view(EditorView.data)(NodeSeq.Empty)
 		case POST(Path("/editor") & Params(data)) =>
 			logger.debug("POST /editor")
-			val splited = EditorView.data("code").apply(0).split("\n")
+			val splited = data("code").head.split("\n")
 			splited.foreach { command =>
-				Eval.inPlace[Any](command)
+				val res = interpreter.interpret(command)
+				//println(res.toString)
+				EditorView.data("interpreter") = EditorView.data("interpreter") :+ res.toString
 			}
-			val vw = EditorView.view(data)_
-			vw(<p> { } </p>)
+			
+			//val res = interpreter.interpret(data("code").head)
+			
+			EditorView.data("code") = data("code")
+			//EditorView.data("interpreter") = EditorView.data("interpreter") :+ res.toString
+			EditorView.view(EditorView.data)(NodeSeq.Empty)
 	}
 }
