b4a5b1793c1f2d7e0e6e66d7b607f39ed66a0139
vascogrilo <vascogrilo@gmail.com>
2012-10-18 14:03:56 +0100

    Added resource file Conversions.scala content description.

--- server/src/main/resources/scala/Conversions.scala
+++ server/src/main/resources/scala/Conversions.scala
@@ -1,3 +1,14 @@
+/**
+ * Object Conversions
+ * In this object we will store all implicit definitions
+ * for converting several data structures into an HTML code
+ * for displaying in a web browser
+ * 
+ * This is intended (for now) as a resource file in which we will store
+ * conversions and then import every conversion in the object
+ * so we can inject this at runtime into the Scala's Interpreter
+ */
+
 object Conversions {
 	
 	implicit def fromList[A](l : List[A]) = new Object {
